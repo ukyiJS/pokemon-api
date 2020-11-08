94b6b3ec3bd0c57d713743ec29858ba267a6b4ce
UkYi <ukyi.js@gmail.com>
2020-11-08 18:45:46 +0900

    feat: app controller, service, resolver 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { CacheService, GraphqlService, TypeormService } from '@/config';
 import { CacheModule, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppResolver } from './app.resolver';
-import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
@@ -14,7 +11,6 @@ import { PokemonModule } from './pokemon/pokemon.module';
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
     PokemonModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, AppResolver],
+  providers: [],
 })
 export class AppModule {}
