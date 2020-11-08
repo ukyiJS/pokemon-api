e70bb8496879d501eb66bbb6a703a555be0d70b6
UkYi <ukyi.js@gmail.com>
2020-11-08 19:00:08 +0900

    test: should return pokemons test 추가

---
+++ src/pokemon/pokemon.service.spec.ts
@@ -0,0 +1,26 @@
+import { TypeormService } from '@/config';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { PokemonService } from './pokemon.service';
+
+describe('PokemonService', () => {
+  let service: PokemonService;
+  let repository: MongoRepository<PokemonOfDatabase>;
+
+  beforeAll(async () => {
+    await Test.createTestingModule({
+      imports: [TypeOrmModule.forRootAsync({ useClass: TypeormService })],
+      providers: [PokemonService, { provide: getRepositoryToken(PokemonOfDatabase), useClass: MongoRepository }],
+    }).compile();
+
+    repository = getMongoRepository(PokemonOfDatabase);
+    service = new PokemonService(repository);
+  });
+
+  it('should return pokemons', async () => {
+    const pokemons = await service.getPokemons();
+    expect(pokemons).not.toHaveLength(0);
+  });
+});
