177b0ccacf1f21067af79bb4e887f878f4239fb4
UkYi <ukyi.js@gmail.com>
2020-11-08 18:46:51 +0900

    feat: getPokemons 추가

--- src/pokemon/pokemon.resolver.ts
+++ src/pokemon/pokemon.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
 import { PokemonService } from './pokemon.service';
 
 @Resolver()
 export class PokemonResolver {
   constructor(private readonly pokemonService: PokemonService) {}
+
+  @Query(() => [PokemonOfDatabase])
+  public async getPokemons(
+    @Args('page', { type: () => Int, nullable: true }) page?: number,
+    @Args('display', { type: () => Int, nullable: true }) display?: number,
+  ): Promise<PokemonOfDatabase[]> {
+    return this.pokemonService.getPokemons(page, display);
+  }
 }
