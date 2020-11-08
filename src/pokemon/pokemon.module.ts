1d5f5c988a1075c3cef7edbb49428ecf38e23bc1
UkYi <ukyi.js@gmail.com>
2020-11-08 18:46:33 +0900

    feat: pokemon controller 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { PokemonController } from './pokemon.controller';
 import { PokemonResolver } from './pokemon.resolver';
 import { PokemonService } from './pokemon.service';
 
@@ -8,5 +9,6 @@ import { PokemonService } from './pokemon.service';
   providers: [PokemonService, PokemonResolver],
   imports: [TypeOrmModule.forFeature([PokemonOfDatabase])],
   exports: [PokemonService],
+  controllers: [PokemonController],
 })
 export class PokemonModule {}
