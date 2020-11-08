114093d8b9c0017f32f28c6577c23b7a51738715
UkYi <ukyi.js@gmail.com>
2020-11-08 18:46:57 +0900

    feat: getPokemons 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MongoRepository } from 'typeorm';
+import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(
+    @InjectRepository(PokemonOfDatabase) private readonly pokemonRepository: MongoRepository<PokemonOfDatabase>,
+  ) {}
+
+  public async getPokemons(page = 1, display = 10): Promise<PokemonOfDatabase[]> {
+    return this.pokemonRepository.find({
+      skip: (page - 1) * display,
+      take: display,
+      order: { no: 'ASC' },
+      cache: true,
+    });
+  }
+}
