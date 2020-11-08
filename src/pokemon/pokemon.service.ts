93200b33704c7673efe4e2b946ead69426718dc9
UkYi <ukyi.js@gmail.com>
2020-11-09 01:12:17 +0900

    feat: getAutoCompleteKeyword 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -35,4 +35,26 @@ export class PokemonService {
       return search(keywordToFilter, this.searchKeyword) > -1;
     });
   }
+
+  public async getAutoCompleteKeyword(keyword: string, page = 1, display = 10): Promise<string[]> {
+    const select = <(keyof PokemonOfDatabase)[]>['name'];
+    if (!this.pokemonNames) {
+      this.pokemonNames = await this.pokemonRepository
+        .find({ select })
+        .then(pokemons => pokemons.map(({ name }) => name));
+    }
+
+    this.searchKeyword = assemble(disassemble(keyword));
+    const matchedNames = isConsonantAll(keyword) ? this.filterByChoSeong() : this.filterByKeyword();
+
+    return this.pokemonRepository
+      .find({
+        select,
+        skip: (page - 1) * display,
+        take: display,
+        cache: true,
+        where: { name: { $in: matchedNames } },
+      })
+      .then(pokemons => pokemons.map(({ name }) => name));
+  }
 }
