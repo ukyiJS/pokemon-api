7a16da74e1583e3c516e51137c769a71355f0beb
UkYi <ukyi.js@gmail.com>
2020-11-08 18:29:06 +0900

    feat: pokemon interface 추가

---
+++ src/pokemon/pokemon.interface.ts
@@ -0,0 +1,58 @@
+export interface IStat {
+  name: string;
+  value: number;
+}
+
+export interface IColor {
+  name: string;
+  code: string;
+}
+
+export interface IGender {
+  name: string;
+  ratio: number;
+}
+
+export interface ITypeDefense {
+  type: string;
+  damage: number;
+}
+
+export interface IEggCycle {
+  cycle: number;
+  step: number[] | null;
+}
+
+export interface IEvolvingTo {
+  no: string;
+  name: string;
+  image: string;
+  form: string | null;
+  condition?: string;
+  evolvingTo: IEvolvingTo[];
+}
+
+export interface IPokemonOfDatabase {
+  no: string;
+  name: string;
+  engName: string;
+  image: string;
+  stats: IStat[];
+  types: string[];
+  typeDefenses: ITypeDefense[];
+  species: string;
+  height: string;
+  weight: string;
+  abilities: string[];
+  hiddenAbility: string | null;
+  evYield: string | null;
+  catchRate: number;
+  friendship: number;
+  exp: number;
+  eegGroups: string[];
+  gender: IGender[];
+  eggCycles: IEggCycle;
+  evolvingTo?: IEvolvingTo[];
+  form: string | null;
+  differentForm: IPokemonOfDatabase[];
+}
