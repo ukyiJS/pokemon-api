9b2c5afe23e1c60036edded2997cf800b3151f6f
UkYi <ukyi.js@gmail.com>
2020-11-08 18:26:31 +0900

    feat: env index 추가

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const envPath = join(process.cwd(), IS_PRODUCTION ? '.env.prod' : '.env.dev');
+
+dotenv.config({ path: envPath });
+
+export const {
+  PORT = 3000,
+  DOMAIN = '',
+  NODE_ENV = 'development',
+  IS_OFFLINE,
+  MONGODB_PORT = 11049,
+  MONGODB_USER = '',
+  MONGODB_PASS = '',
+  MONGODB_HOST = '',
+  MONGODB_DATABASE = '',
+} = process.env;
+
+export const MONGODB_ATLAS_URL = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}${MONGODB_HOST}/${MONGODB_DATABASE}`;
+export const MONGODB_URL = process.env.MONGODB_PORT ? `mongodb://localhost:${MONGODB_PORT}` : MONGODB_ATLAS_URL;
