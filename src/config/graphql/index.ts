dd44dc21c2b1ad14b4131262c6382a352ce5fbee
UkYi <ukyi.js@gmail.com>
2020-11-10 01:37:25 +0900

    feat: autoSchemaFile true 설정 추가

--- src/config/graphql/index.ts
+++ src/config/graphql/index.ts
@@ -1,9 +1,8 @@
-import { IS_OFFLINE, IS_PRODUCTION } from '@/env';
 import { Injectable } from '@nestjs/common';
 import { GqlModuleOptions, GqlOptionsFactory } from '@nestjs/graphql';
 import { GraphQLError, GraphQLFormattedError } from 'graphql';
-import { join } from 'path';
 import { ObjectLiteral } from 'typeorm';
+import { IS_OFFLINE, IS_PRODUCTION } from '../../env';
 
 @Injectable()
 export class GraphqlService implements GqlOptionsFactory {
@@ -15,13 +14,11 @@ export class GraphqlService implements GqlOptionsFactory {
   }
 
   private getProdOptions = (): GqlModuleOptions => ({
-    typePaths: ['dist/*.gql'],
     cacheControl: { defaultMaxAge: 5, stripFormattedExtensions: false, calculateHttpHeaders: false },
   });
 
   private getDevOptions = (): GqlModuleOptions => ({
     path: !IS_OFFLINE ? this.endpoint : undefined,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     tracing: true,
   });
 
@@ -29,6 +26,7 @@ export class GraphqlService implements GqlOptionsFactory {
     return {
       ...this.options,
       cors: true,
+      autoSchemaFile: true,
       playground: { endpoint: this.endpoint },
       introspection: true,
       formatError: (error: GraphQLError): GraphQLFormattedError & ObjectLiteral => ({
