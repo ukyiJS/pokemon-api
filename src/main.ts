a87da1b85420f67a10fcb444a78f5fb04275ac3a
UkYi <ukyi.js@gmail.com>
2020-11-09 19:43:25 +0900

    feat: useGlobalFilters, useGlobalPipes 추가

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-import { LoggingInterceptor, TimeoutInterceptor } from '@/common';
-import { PORT } from '@/env';
-import { validateEnv } from '@/utils';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter, LoggingInterceptor, TimeoutInterceptor } from './common';
+import { PORT } from './env';
+import { validateEnv } from './utils';
 
-const createServer = async () => {
+const bootstrapServer = async () => {
   validateEnv();
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor()).listen(PORT);
+  await app
+    .useGlobalFilters(new HttpExceptionFilter())
+    .useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor())
+    .useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }))
+    .listen(PORT);
 
   Logger.log(`Server running on http://localhost:${PORT}`, 'Server');
 };
-createServer();
+bootstrapServer();
