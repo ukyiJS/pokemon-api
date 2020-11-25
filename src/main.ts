1e5f7ccc1c2bc7ff6e983327a80972c5845113db
ukyiJS <ukyi.js@gmail.com>
2020-11-25 21:54:01 +0900

    feat: useGlobalFilters, useGlobalInterceptors, useGlobalPipes 제거

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter, LoggingInterceptor, TimeoutInterceptor } from './common';
 import { PORT } from './env';
 import { validateEnv } from './utils';
 
 const bootstrapServer = async () => {
   validateEnv();
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app
-    .useGlobalFilters(new HttpExceptionFilter())
-    .useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor())
-    .useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }))
-    .listen(PORT);
-
+  await (await NestFactory.create(AppModule, { cors: true })).listen(PORT);
   Logger.log(`Server running on http://localhost:${PORT}`, 'Server');
 };
 bootstrapServer();
