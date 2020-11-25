0c9efa774974a91dce651a146c860cad7a53a742
ukyiJS <ukyi.js@gmail.com>
2020-11-25 14:46:41 +0900

    feat: CacheModule 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { CacheService, GraphqlService, TypeormService } from './config';
+import { GraphqlService, TypeormService } from './config';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
-    CacheModule.registerAsync({ useClass: CacheService }),
     GraphQLModule.forRootAsync({ useClass: GraphqlService }),
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
     PokemonModule,
