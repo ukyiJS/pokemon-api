17b06d775ebc6e37245d9ab4d53a6ce2ca9733a3
ukyiJS <ukyi.js@gmail.com>
2020-11-25 21:54:54 +0900

    feat: ValidationPipe 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -6,6 +7,7 @@ import { GraphqlService, TypeormService } from './config';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
+  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
   imports: [
     GraphQLModule.forRootAsync({ useClass: GraphqlService }),
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
