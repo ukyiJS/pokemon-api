08521d801752a7acdb3e89374fe1e98bbe0d9c34
ukyiJS <ukyi.js@gmail.com>
2020-11-25 14:48:08 +0900

    feat: CacheModule 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheService } from 'src/config';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
 import { PokemonResolver } from './pokemon.resolver';
 import { PokemonService } from './pokemon.service';
 
 @Module({
   providers: [PokemonService, PokemonResolver],
-  imports: [TypeOrmModule.forFeature([PokemonOfDatabase])],
+  imports: [CacheModule.registerAsync({ useClass: CacheService }), TypeOrmModule.forFeature([PokemonOfDatabase])],
   exports: [PokemonService],
 })
 export class PokemonModule {}
