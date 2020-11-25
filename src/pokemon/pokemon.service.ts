c9ef852c67a08b5795e0ad1ef0080fb402276ee9
ukyiJS <ukyi.js@gmail.com>
2020-11-25 15:44:43 +0900

    feat: getAutoCompleteKeyword 추가

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,14 @@
-import { CacheStore, CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
+import { CacheStore, CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { AutoCompleteUtil } from '../utils/autoComplete';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
+import { PokemonNames } from './pokemon.type';
 
 @Injectable()
 export class PokemonService {
+  private pokemonNames: PokemonNames;
+
   constructor(
     @InjectRepository(PokemonOfDatabase) private readonly pokemonRepository: MongoRepository<PokemonOfDatabase>,
     @Inject(CACHE_MANAGER) private readonly cacheManager: CacheStore,
@@ -20,19 +23,30 @@ export class PokemonService {
     });
   }
 
-  public async getAutoCompleteKeyword(keyword: string, page = 1, display = 10): Promise<string[]> {
-    const { getSearchType, getMatchedTexts } = new AutoCompleteUtil(this.pokemonRepository, this.cacheManager);
-    const matchedTexts = await getMatchedTexts(keyword);
-    const searchType = getSearchType();
-
-    return this.pokemonRepository
-      .find({
-        select: [searchType],
-        skip: (page - 1) * display,
-        take: display,
+  public async getAutoCompleteKeyword(keyword: string, display = 10): Promise<string[]> {
+    if (!this.pokemonNames?.kor) {
+      const pokemons = await this.pokemonRepository.find({
+        select: ['name', 'engName', 'searchCount'],
         cache: true,
-        where: { [searchType]: { $in: matchedTexts } },
-      })
-      .then(pokemons => pokemons.map(pokemon => pokemon[searchType]));
+      });
+
+      const pokemonNames = pokemons.reduce<PokemonNames>(
+        ({ kor, eng, count }, { name, engName, searchCount }) => ({
+          kor: [...kor, name],
+          eng: [...eng, engName],
+          count: [...count, searchCount],
+        }),
+        { kor: [], eng: [], count: [] },
+      );
+      await this.cacheManager.set('pokemonNames', pokemonNames, { ttl: 50000 });
+      this.pokemonNames = (await this.cacheManager.get<PokemonNames>('pokemonNames'))!;
+    }
+
+    const { getAutoCompleteKeyword } = new AutoCompleteUtil(this.pokemonNames);
+
+    return getAutoCompleteKeyword(keyword)
+      .filter((_, i) => i <= display)
+      .sort((a, b) => a.count - b.count)
+      .map(({ result }) => result);
   }
 }
