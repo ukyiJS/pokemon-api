faaf1bf519a65d0ccd2af99ae5ee602d40cf531b
ukyiJS <ukyi.js@gmail.com>
2020-11-25 19:17:37 +0900

    feat: IPokemonOfDatabase interfaceType 추가

--- src/pokemon/pokemon.interface.ts
+++ src/pokemon/pokemon.interface.ts
@@ -1,3 +1,12 @@
+import { Field, Float, Int, InterfaceType } from '@nestjs/graphql';
+import { Column } from 'typeorm';
+import { DifferentForm } from './model/differentForm.entity';
+import { EggCycle } from './model/eggCycle.entity';
+import { EvolvingTo } from './model/evolvingTo.entity';
+import { Gender } from './model/gender.entity';
+import { Stat } from './model/stat.entity';
+import { TypeDefense } from './model/typeDefense.entity';
+
 export interface IStat {
   name: string;
   value: number;
@@ -32,28 +41,105 @@ export interface IEvolvingTo {
   evolvingTo: IEvolvingTo[];
 }
 
-export interface IPokemonOfDatabase {
+@InterfaceType()
+export abstract class IPokemonOfDatabase {
+  @Column({ unique: true })
+  @Field()
   no: string;
+
+  @Column()
+  @Field()
   name: string;
+
+  @Column()
+  @Field()
   engName: string;
+
+  @Column()
+  @Field()
   image: string;
+
+  @Column()
+  @Field({ nullable: true })
   icon?: string;
-  stats: IStat[];
+
+  @Column()
+  @Field(() => [Stat])
+  stats: Stat[];
+
+  @Column()
+  @Field(() => [String])
   types: string[];
-  typeDefenses: ITypeDefense[];
+
+  @Column()
+  @Field(() => [TypeDefense])
+  typeDefenses: TypeDefense[];
+
+  @Column()
+  @Field()
   species: string;
+
+  @Column()
+  @Field()
   height: string;
+
+  @Column()
+  @Field()
   weight: string;
+
+  @Column()
+  @Field(() => [String])
   abilities: string[];
-  hiddenAbility: string | null;
-  evYield: string | null;
+
+  @Column()
+  @Field({ nullable: true })
+  hiddenAbility: string;
+
+  @Column()
+  @Field({ nullable: true })
+  evYield: string;
+
+  @Column()
+  @Field(() => Int)
   catchRate: number;
+
+  @Column()
+  @Field(() => Int)
   friendship: number;
+
+  @Column()
+  @Field(() => Int)
   exp: number;
+
+  @Column()
+  @Field(() => [String])
   eegGroups: string[];
-  gender: IGender[];
-  eggCycles: IEggCycle;
-  evolvingTo?: IEvolvingTo[];
-  form: string | null;
-  differentForm: IPokemonOfDatabase[];
+
+  @Column()
+  @Field(() => [Gender])
+  gender: Gender[];
+
+  @Column()
+  @Field(() => EggCycle)
+  eggCycles: EggCycle;
+
+  @Column()
+  @Field({ nullable: true })
+  form: string;
+
+  @Column()
+  @Field(() => [EvolvingTo])
+  evolvingTo: EvolvingTo[];
+
+  @Column()
+  @Field(() => [DifferentForm])
+  differentForm: DifferentForm[];
+
+  @Column()
+  @Field(() => Float)
+  createdAt?: number;
+
+  @Column()
+  @Field(() => Int)
+  searchCount?: number;
 }
