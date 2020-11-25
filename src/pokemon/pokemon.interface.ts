5a2ea25bd4eaea7baf78ad3c58dd252b6fc76838
ukyiJS <ukyi.js@gmail.com>
2020-11-25 19:58:40 +0900

    feat: Column 데코레이터 제거

--- src/pokemon/pokemon.interface.ts
+++ src/pokemon/pokemon.interface.ts
@@ -1,5 +1,4 @@
 import { Field, Float, Int, InterfaceType } from '@nestjs/graphql';
-import { Column } from 'typeorm';
 import { DifferentForm } from './model/differentForm.entity';
 import { EggCycle } from './model/eggCycle.entity';
 import { EvolvingTo } from './model/evolvingTo.entity';
@@ -43,103 +42,81 @@ export interface IEvolvingTo {
 
 @InterfaceType()
 export abstract class IPokemonOfDatabase {
-  @Column({ unique: true })
+  @Field()
+  _id?: string;
+
   @Field()
   no: string;
 
-  @Column()
   @Field()
   name: string;
 
-  @Column()
   @Field()
   engName: string;
 
-  @Column()
   @Field()
   image: string;
 
-  @Column()
   @Field({ nullable: true })
   icon?: string;
 
-  @Column()
   @Field(() => [Stat])
   stats: Stat[];
 
-  @Column()
   @Field(() => [String])
   types: string[];
 
-  @Column()
   @Field(() => [TypeDefense])
   typeDefenses: TypeDefense[];
 
-  @Column()
   @Field()
   species: string;
 
-  @Column()
   @Field()
   height: string;
 
-  @Column()
   @Field()
   weight: string;
 
-  @Column()
   @Field(() => [String])
   abilities: string[];
 
-  @Column()
   @Field({ nullable: true })
   hiddenAbility: string;
 
-  @Column()
   @Field({ nullable: true })
   evYield: string;
 
-  @Column()
   @Field(() => Int)
   catchRate: number;
 
-  @Column()
   @Field(() => Int)
   friendship: number;
 
-  @Column()
   @Field(() => Int)
   exp: number;
 
-  @Column()
   @Field(() => [String])
   eegGroups: string[];
 
-  @Column()
   @Field(() => [Gender])
   gender: Gender[];
 
-  @Column()
   @Field(() => EggCycle)
   eggCycles: EggCycle;
 
-  @Column()
   @Field({ nullable: true })
   form: string;
 
-  @Column()
   @Field(() => [EvolvingTo])
   evolvingTo: EvolvingTo[];
 
-  @Column()
   @Field(() => [DifferentForm])
   differentForm: DifferentForm[];
 
-  @Column()
   @Field(() => Float)
   createdAt?: number;
 
-  @Column()
   @Field(() => Int)
   searchCount?: number;
 }
