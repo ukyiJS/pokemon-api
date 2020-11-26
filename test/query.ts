8f32d40152e99a1ca11e538e75aff3195f3189ba
ukyiJS <ukyi.js@gmail.com>
2020-11-27 00:51:46 +0900

    feat: getPokemonQuery 추가

--- test/query.ts
+++ test/query.ts
@@ -99,3 +99,104 @@ export const getPokemonsQuery = `
     form
   }
 `;
+
+export const getPokemonQuery = `
+  {
+    getPokemon(pokemonName: "이상해씨") {
+      no
+      engName
+      image
+      icon
+      types
+      stats {
+        name
+        value
+      }
+      types
+      typeDefenses {
+        type
+        damage
+      }
+      species
+      height
+      weight
+      abilities
+      hiddenAbility
+      evYield
+      catchRate
+      friendship
+      exp
+      eegGroups
+      gender {
+        name
+        ratio
+      }
+      eggCycles {
+        cycle
+        step
+      }
+      form
+      differentForm {
+        ...DifferentForm
+      }
+      evolvingTo {
+        ...EvolvingTo
+      }
+      createdAt
+      searchCount
+    }
+  }
+
+  fragment PokemonOfEvolvingTo on EvolvingTo {
+    no
+    name
+    image
+    form
+    condition
+  }
+
+  fragment EvolvingTo on EvolvingTo {
+    ...PokemonOfEvolvingTo
+    evolvingTo {
+      ...PokemonOfEvolvingTo
+      evolvingTo {
+        ...PokemonOfEvolvingTo
+      }
+    }
+  }
+
+  fragment DifferentForm on DifferentForm {
+    no
+    engName
+    image
+    types
+    stats {
+      name
+      value
+    }
+    types
+    typeDefenses {
+      type
+      damage
+    }
+    species
+    height
+    weight
+    abilities
+    hiddenAbility
+    evYield
+    catchRate
+    friendship
+    exp
+    eegGroups
+    gender {
+      name
+      ratio
+    }
+    eggCycles {
+      cycle
+      step
+    }
+    form
+  }
+`;
