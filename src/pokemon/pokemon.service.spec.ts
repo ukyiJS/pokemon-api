b5d10726a9df36e33740d03506180debb8df2718
ukyiJS <ukyi.js@gmail.com>
2020-11-27 00:43:33 +0900

    test: getAutoCompleteKeyword tset 추가

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -7,6 +7,7 @@ import { PokemonArgs } from './args/pokemon.args';
 import { PokemonOfDatabase } from './model/pokemonOfDatabase.entity';
 import { PokemonService } from './pokemon.service';
 
+jest.setTimeout(10000);
 describe('PokemonService', () => {
   let service: PokemonService;
   let repository: MongoRepository<PokemonOfDatabase>;
@@ -24,13 +25,18 @@ describe('PokemonService', () => {
     service = new PokemonService(repository);
   });
 
-  it.only('should return pokemon', async () => {
+  it('should return pokemon', async () => {
     const pokemon = await service.getPokemon('이브이');
     expect(pokemon).not.toBeUndefined();
   });
 
   it('should return pokemons', async () => {
     const pokemons = await service.getPokemons(<PokemonArgs>{});
+    expect(pokemons).not.toHaveLength(0);
+  });
+
+  it.only('should return autoComplete keyword', async () => {
+    const pokemons = await service.getAutoCompleteKeyword({ keyword: 'ㅇㅂㅇ', display: 10 });
     expect(pokemons).not.toHaveLength(0);
   });
 });
