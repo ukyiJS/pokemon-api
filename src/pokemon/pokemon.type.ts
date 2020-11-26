a4d5ec0ad75b98eef6b7d3abe626553b820c4d32
ukyiJS <ukyi.js@gmail.com>
2020-11-26 14:48:07 +0900

    feat: PokemonTypes enum 추가

--- src/pokemon/pokemon.type.ts
+++ src/pokemon/pokemon.type.ts
@@ -1,3 +1,5 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum SearchTypes {
   KOR = 'kor',
   ENG = 'eng',
@@ -20,3 +22,26 @@ export type Entries<T> = {
 }[keyof T][];
 
 export type FindCondition = { $all: string[] } | { [key: string]: RegExp };
+
+export enum PokemonTypes {
+  NORMAL = '노말',
+  FIRE = '불',
+  WATER = '물',
+  ELECTRIC = '전기',
+  GRASS = '풀',
+  ICE = '얼음',
+  FIGHTING = '격투',
+  POISON = '독',
+  GROUND = '땅',
+  FLYING = '비행',
+  PSYCHIC = '에스퍼',
+  BUG = '벌레',
+  ROCK = '바위',
+  GHOST = '고스트',
+  DRAGON = '드래곤',
+  DARK = '악',
+  STEEL = '강철',
+  FAIRY = '페어리',
+}
+export type PokemonType = typeof PokemonTypes[keyof typeof PokemonTypes];
+registerEnumType(PokemonTypes, { name: 'PokemonTypes' });
