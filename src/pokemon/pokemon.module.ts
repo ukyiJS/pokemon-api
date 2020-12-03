f0c36db9281bfa63ed78d2f7cf36748d48a72cab
ukyiJS <ukyi.js@gmail.com>
2020-12-04 02:42:57 +0900

    feat: Sessions 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -2,12 +2,16 @@ import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CacheService } from '../config';
 import { PokemonDatabase } from './model/pokemonDatabase.entity';
+import { Sessions } from './model/session.entity';
 import { PokemonResolver } from './pokemon.resolver';
 import { PokemonService } from './pokemon.service';
 
 @Module({
   providers: [PokemonService, PokemonResolver],
-  imports: [CacheModule.registerAsync({ useClass: CacheService }), TypeOrmModule.forFeature([PokemonDatabase])],
+  imports: [
+    CacheModule.registerAsync({ useClass: CacheService }),
+    TypeOrmModule.forFeature([PokemonDatabase, Sessions]),
+  ],
   exports: [PokemonService],
 })
 export class PokemonModule {}
