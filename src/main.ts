7d917b3971500e4da5efe8a5be3646deecf9c854
ukyiJS <ukyi.js@gmail.com>
2020-12-04 02:43:48 +0900

    feat: sessionStore 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sessionStore } from './config/session';
 import { PORT } from './env';
 import { validateEnv } from './utils';
 
 const bootstrapServer = async () => {
   validateEnv();
-  await (await NestFactory.create(AppModule, { cors: true })).listen(PORT);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.use(sessionStore).listen(PORT);
+
   Logger.log(`Server running on http://localhost:${PORT}`, 'Server');
 };
 bootstrapServer();
