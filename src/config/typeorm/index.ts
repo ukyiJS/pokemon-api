174a86dd15daa2aad493a29b51754735c370acc1
ukyiJS <ukyi.js@gmail.com>
2020-12-04 19:17:28 +0900

    feat: configService 추가

--- src/config/typeorm/index.ts
+++ src/config/typeorm/index.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { join } from 'path';
-import { MONGODB_URL } from '../../env';
 
 @Injectable()
 export class TypeormService implements TypeOrmOptionsFactory {
+  private readonly configService: ConfigService;
+
   async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
     return {
       type: 'mongodb',
-      url: MONGODB_URL,
+      url: this.configService.get('database.url'),
       entities: [join(__dirname, '../../', '**/**.entity{.ts,.js}')],
       synchronize: true,
       useNewUrlParser: true,
