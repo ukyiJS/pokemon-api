79de613ecd51740460755fa34edf2da29350f891
ukyiJS <ukyi.js@gmail.com>
2020-12-04 19:14:43 +0900

    feat: createSessionStore 추가

--- src/config/session/index.ts
+++ src/config/session/index.ts
@@ -1,16 +1,22 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import ConnectMongo from 'connect-mongodb-session';
+import express from 'express';
 import session from 'express-session';
-import { MONGODB_URL, SESSION_SECRET } from '../../env';
 
-const MongoStore = ConnectMongo(session);
-const store = new MongoStore({ uri: MONGODB_URL, collection: 'sessions' });
-store.on('error', (error: Error) => Logger.error(error.message, error.stack, error.name));
+export const createSessionStore = (app: INestApplication): express.RequestHandler => {
+  const config = app.get(ConfigService);
+  const url = config.get('database.url');
+  const secret = config.get('sessionSecret');
+  const MongoStore = ConnectMongo(session);
+  const store = new MongoStore({ uri: url, collection: 'sessions' });
+  store.on('error', (error: Error) => Logger.error(error.message, error.stack, error.name));
 
-export const sessionStore = session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 },
-});
+  return session({
+    secret,
+    resave: false,
+    saveUninitialized: false,
+    store,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  });
+};
