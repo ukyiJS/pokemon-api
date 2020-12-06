fad9f9861f8f878f130970fb161955ea8539a5ac
UkYiJS <ukyi.js@gmail.com>
2020-12-06 23:30:13 +0900

    feat: ResolveField abilities 제거

--- src/pokemon/resolvers/pokemonDatabase.resolver.ts
+++ src/pokemon/resolvers/pokemonDatabase.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { PokemonArgs } from '../args/pokemon.args';
 import { PokemonDatabase } from '../model/pokemonDatabase.entity';
 import { PokemonService } from '../pokemon.service';
@@ -15,10 +15,5 @@ export class PokemonDatabaseResolver {
   @Query(() => [PokemonDatabase])
   public async getPokemons(@Args({ nullable: true }) pokemonArgs: PokemonArgs): Promise<PokemonDatabase[]> {
     return this.pokemonService.getPokemons(pokemonArgs);
-  }
-
-  @ResolveField(() => [String])
-  public abilities(@Parent() { abilities }: PokemonDatabase): string[] {
-    return abilities.filter(ability => ability);
   }
 }
