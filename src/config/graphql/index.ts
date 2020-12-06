e76ecaa4ed40804198fbc848fd2469dfa5691f73
UkYiJS <ukyi.js@gmail.com>
2020-12-06 15:54:20 +0900

    feat: config 추가

--- src/config/graphql/index.ts
+++ src/config/graphql/index.ts
@@ -1,32 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GqlModuleOptions, GqlOptionsFactory, registerEnumType } from '@nestjs/graphql';
 import { GraphQLError, GraphQLFormattedError } from 'graphql';
-import { PokemonTypes } from 'src/pokemon/enums/pokemonType.enum';
+import { Environment, Environments } from 'src/pokemon/enums/environment.enum';
 import { ObjectLiteral } from 'typeorm';
-import { IS_OFFLINE, IS_PRODUCTION } from '../../env';
+import { PokemonTypes } from '../../pokemon/enums/pokemonType.enum';
+import { IEnvironment } from '../env';
+
+type GraphQLOptions = { [mode in Exclude<Environment, Environments.TEST>]: GqlModuleOptions };
 
 @Injectable()
 export class GraphqlService implements GqlOptionsFactory {
   private readonly endpoint = '/api/graphql';
-  private options: GqlModuleOptions;
+  private mode: keyof GraphQLOptions;
+  private options: GraphQLOptions;
 
-  constructor() {
-    this.options = IS_PRODUCTION ? this.getProdOptions() : this.getDevOptions();
+  constructor(private readonly configService: ConfigService) {
+    this.mode = <keyof GraphQLOptions>this.config('node');
+    this.options = {
+      development: { path: !this.config('isOffline') ? this.endpoint : undefined, tracing: true },
+      production: { cacheControl: { defaultMaxAge: 5, stripFormattedExtensions: false, calculateHttpHeaders: false } },
+    };
     registerEnumType(PokemonTypes, { name: 'PokemonTypes' });
   }
 
-  private getProdOptions = (): GqlModuleOptions => ({
-    cacheControl: { defaultMaxAge: 5, stripFormattedExtensions: false, calculateHttpHeaders: false },
-  });
-
-  private getDevOptions = (): GqlModuleOptions => ({
-    path: !IS_OFFLINE ? this.endpoint : undefined,
-    tracing: true,
-  });
+  private config = <T extends keyof IEnvironment>(key: T): IEnvironment[T] => {
+    return this.configService.get<IEnvironment[typeof key]>(key)!;
+  };
 
   public createGqlOptions = async (): Promise<GqlModuleOptions> => {
     return {
-      ...this.options,
+      ...this.options[this.mode],
       cors: true,
       autoSchemaFile: true,
       playground: { endpoint: this.endpoint },
