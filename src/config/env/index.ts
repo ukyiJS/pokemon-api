5126615c7559c2f4b43328dbce9c11bf362f8d5f
UkYiJS <ukyi.js@gmail.com>
2020-12-06 15:52:01 +0900

    feat: envConfig 추가

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,15 +1,21 @@
+import { Environment } from 'src/pokemon/enums/environment.enum';
+
 export interface DatabaseEnv {
   url: string;
 }
-export interface Env {
+export interface IEnvironment {
+  node: Environment;
   port: number;
+  isOffline?: string;
   sessionSecret: string;
   database: DatabaseEnv;
 }
 
-export const envConfig = (): Env => ({
+export const envConfig = (): IEnvironment => ({
+  node: <Environment>process.env.NODE_ENV!,
   port: +process.env.PORT!,
   sessionSecret: process.env.SESSION_SECRET!,
+  isOffline: process.env.IS_OFFLINE,
   database: {
     url: process.env.DATABASE_URL!,
   },
