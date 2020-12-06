46a0ec56d4e0e98441428eccb256a845406f9f88
UkYiJS <ukyi.js@gmail.com>
2020-12-06 18:50:32 +0900

    feat: AutoCompleteModule 추가

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,16 +1,14 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AutoCompleteModule } from 'src/utils';
 import { CacheService } from '../config';
 import { PokemonDatabase } from './model/pokemonDatabase.entity';
 import { Sessions } from './model/session.entity';
-import * as resolvers from './resolvers';
 import { PokemonService } from './pokemon.service';
+import * as resolvers from './resolvers';
 
 @Module({
   providers: [PokemonService, ...Object.values(resolvers)],
-  imports: [
-    CacheModule.registerAsync({ useClass: CacheService }),
-    TypeOrmModule.forFeature([PokemonDatabase, Sessions]),
-  ],
+  imports: [TypeOrmModule.forFeature([PokemonDatabase, Sessions]), CacheModule.registerAsync({ useClass: CacheService }), AutoCompleteModule],
 })
 export class PokemonModule {}
